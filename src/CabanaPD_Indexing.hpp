978fde9bc09570301b959197fafbf51f7695345e
Jakob Bludau <bludauj@ornl.gov>
2025-11-21 14:30:26 -0500

    Add a static assert for diagonal indexing with size 0

--- src/CabanaPD_Indexing.hpp
+++ src/CabanaPD_Indexing.hpp
@@ -17,6 +17,8 @@ namespace CabanaPD
 template <unsigned NumBaseModels>
 struct DiagonalIndexing
 {
+    static_assert(NumBaseModels > 0, "NumBaseModels must be larger than 0");
+
     KOKKOS_FUNCTION unsigned operator()( unsigned firstType,
                                          unsigned secondType ) const
     {
